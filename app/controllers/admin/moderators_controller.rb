966118f59cbce9628c3e74c424fee3a226d13044
YishunHa <u1650778@unimail.hud.ac.uk>
2017-04-13 23:31:55 +0100

    add the update to the moderator

--- app/controllers/admin/moderators_controller.rb
+++ app/controllers/admin/moderators_controller.rb
@@ -8,6 +8,19 @@ class Admin::ModeratorsController < ApplicationController
   end
 
   def update
-    
+    @moderator = Moderator.find(params[:id])
+    byebug
+    if @moderator.update(moderator_params)
+      redirect_to :back
+    else
+      render 'edit'
+    end
   end
+
+
+
+  def moderator_params
+    params.require(:moderator).permit(:id, :fullname, :username, :password)
+  end
+
 end
