f80a4893a8a106ff1304b6ff4cd7ab39e4279d05
YishunHa <u1650778@unimail.hud.ac.uk>
2017-04-13 13:49:03 +0100

    build up the database migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+moderator = Moderator.create(
+  fullname: "Yishun Xia",
+  username: "yal0858@gmail.com",
+  password: "x12345"
+)
