76e830f8be3f070be87307673578bb9fa7df8d88
YishunHa <u1650778@unimail.hud.ac.uk>
2017-04-15 00:42:56 +0100

    add the post page in the back end

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,4 +1,8 @@
-class Admin::ApplicationController < ApplicationController
+class Admin::ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  layout 'admin'
+
   before_action :authorize
 
   def current_moderator
