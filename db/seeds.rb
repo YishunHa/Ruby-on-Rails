d06bee9e90e38dd913a6e2fd6b16aff8cd791e8d
YishunHa <u1650778@unimail.hud.ac.uk>
2017-04-15 04:00:55 +0100

    add the pagize funciton and new post create

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,17 @@ moderator = Moderator.create(
   username: "yal0858@gmail.com",
   password: "x12345"
 )
+
+30.times do
+post = Post.create(
+  title: Faker::Lorem.sentence(20),
+  content: Faker::Lorem.paragraph,
+  publish: true,
+  moderator: moderator
+)
+
+tag = Tag.create(name: Faker::Lorem.word)
+
+post_tag = PostTag.create(post: post, tag: tag)
+
+end
