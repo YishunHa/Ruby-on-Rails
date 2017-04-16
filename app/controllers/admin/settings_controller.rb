05450adfabe34e691a25b47d349724c41660e9e3
YishunHa <u1650778@unimail.hud.ac.uk>
2017-04-16 20:29:03 +0100

    finish the back end admin construction

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,19 +1,39 @@
 class Admin::SettingsController < Admin::ApplicationController
   def new
+    if Setting.any?
+      redirect_to edit_admin_setting_url(Setting.first)
+    else
     @setting = Setting.new
+    end
   end
 
   def create
+    @setting = Setting.new(settings_params)
+    if @setting.save
+    redirect_to edit_admin_setting_url(@setting), notice: 'Setting created successfully'
+  else
+    flash[:alert] = 'Some problems occur!'
+    render :new
+    end
   end
 
   def edit
+    @setting = Setting.find(params[:id])
+
   end
 
   def update
+    @setting = Setting.find(params[:id])
+    if @setting.update(settings_params)
+      redirect_to edit_admin_setting_url(@setting), notice: 'Setting successfully updated'
+    else
+      flash[:alert] = 'Some problems occur'
+      render :edit
+    end
   end
 
   private
   def settings_params
-    params.require(:setting).permit(:id, :site_name, :post_per_page, :under_maintenance, :prevent_commenting, :tag_visibility)
+    params.require(:setting).permit(:id, :sitename, :post_per_page, :under_maintenance, :prevent_commenting, :tag_visibility)
   end
 end
