d4f64d3f487fc10c26cc0573ea18df84415a3759
YishunHa <u1650778@unimail.hud.ac.uk>
2017-04-18 04:29:34 +0100

    now the visitors can commit their comments to the post
    the comment will display only if the admin approve the comment in the dashboard

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  belongs_to :visitor
+  belongs_to :visitor, required: false
   has_many :notifications, as: :notifiable, dependent: :destroy
+
+ scope :approved, -> { where status: true }
+
 
   def status_show string
     if string == 'true'
