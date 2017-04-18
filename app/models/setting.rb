b6b1ad92f5f351b2689e4fc8643645951601a46b
YishunHa <u1650778@unimail.hud.ac.uk>
2017-04-18 01:51:46 +0100

    add the comment to post

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,14 +1,14 @@
 class Setting < ApplicationRecord
   def self.site_name
-    Setting.first.sitename
+    Setting.first.try(:sitename)
   end
 
   def self.post_per_page
-    Setting.first.post_per_page
+    Setting.first.try(:post_per_page)
   end
 
   def self.tag_visible?
-    Setting.first.tag_visibility
+    Setting.first.try(:tag_visibility)
   end
 
 end
