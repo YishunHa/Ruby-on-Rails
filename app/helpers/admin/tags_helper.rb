a3c3487871254c19917c82480beb1fa7787bf481
YishunHa <u1650778@unimail.hud.ac.uk>
2017-04-21 09:13:10 +0100

    fix some ui bugs and add SSL to the server

--- app/helpers/admin/tags_helper.rb
+++ app/helpers/admin/tags_helper.rb
@@ -2,11 +2,11 @@ module Admin::TagsHelper
   def create_deletable_button tag
     if tag.in_use?
       link_to '#' do
-      content_tag(:button, 'Delete', class: 'disabled')
+      content_tag(:button, 'Delete', class: 'btn btn-danger', disabled:"disabled")
       end
        else
        link_to admin_tag_path(tag), method: :DELETE do
-      content_tag(:button, 'Delete')
+      content_tag(:button, 'Delete', class: 'btn btn-danger')
       end
      end
   end
--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module RubyOnRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.force_ssl = (ENV["ENABLE_HTTPS"] == "yes")
   end
 end
